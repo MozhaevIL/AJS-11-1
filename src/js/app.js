305f577b93fd462e8db55e00b03b2072a0f7bc18
MozhaevIL <ptitzzz@gmail.com>
2020-08-03 13:22:17 +0300

    initial commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,27 +1,25 @@
-export default class Settings {
-  constructor(settingsInput) { // принимает объект, содержащий пользовательские настройки
-    this.default = new Map([
-      ['theme', 'dark'],
-      ['music', 'trance'],
-      ['difficulty', 'easy'],
-    ]);
-    this.userSettings = new Map();
-    for (const option in settingsInput) {
-      if (Object.prototype.hasOwnProperty.call(settingsInput, option)) {
-        this.userSettings.set(option, settingsInput[option]);
+class Team {
+  constructor(...characters) {
+    this.characters = characters;
+  }
+  [Symbol.iterator]() {
+    let list = this.characters;
+    let current = 0;
+    let last = this.characters.length - 1;
+    return {
+      next(){
+        console.log(current, last);
+        if( current > last ) {
+          return {
+            done: true,
+          };
+        } else {
+          return {
+            done: false,
+            value: list[current++],
+          };
+        }
       }
     }
-  }
-
-  get settings() {
-    const mergedSettings = new Map();
-    this.default.forEach((value, key) => {
-      if (this.userSettings.has(key)) {
-        mergedSettings.set(key, this.userSettings.get(key));
-      } else {
-        mergedSettings.set(key, value);
-      }
-    });
-    return mergedSettings;
   }
 }
