b5ab1e1e17787e62cee14dc8c600bcfe21c94ed3
MozhaevIL <ptitzzz@gmail.com>
2020-08-03 13:38:02 +0300

    added solution

--- README.md
+++
@@ -1,1 +0,0 @@
-[![Build status](https://ci.appveyor.com/api/projects/status/1nkceqy402r8va4k?svg=true)](https://ci.appveyor.com/project/MozhaevIL/ajs-8-3)
--- src/js/app.js
+++ src/js/app.js
@@ -1,25 +1,25 @@
-class Team {
+export default class Team {
   constructor(...characters) {
     this.characters = characters;
   }
+
   [Symbol.iterator]() {
-    let list = this.characters;
+    const list = this.characters;
     let current = 0;
-    let last = this.characters.length - 1;
+    const last = this.characters.length - 1;
     return {
-      next(){
-        console.log(current, last);
-        if( current > last ) {
+      next() {
+        if (current > last) {
           return {
             done: true,
           };
-        } else {
-          return {
-            done: false,
-            value: list[current++],
-          };
         }
-      }
-    }
+        current += 1;
+        return {
+          done: false,
+          value: list[current - 1],
+        };
+      },
+    };
   }
 }
